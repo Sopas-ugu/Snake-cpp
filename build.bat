de9efa168ea2471fa57be083f13c68c918d2b609
Sopas <91762103+Sopas-ugu@users.noreply.github.com>
2022-02-11 16:18:58 -0300

    Tiny fix

--- build.bat
+++ build.bat
@@ -2,6 +2,5 @@
 title Build
 echo Compiling
 g++ main.cpp .\src\* -Iinclude\ -o Snake -IC:\SDL2\x86_64-w64-mingw32\include -LC:\SDL2\x86_64-w64-mingw32\lib -lmingw32 -lSDL2main -lSDL2
-color 0a
 pause
 exit
